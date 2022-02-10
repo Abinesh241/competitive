7c981f2da4ec6cade67767f171e99831121d0a93
Abinesh241 <97597694+Abinesh241@users.noreply.github.com>
2022-02-10 12:06:03 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -8,7 +8,7 @@ public class test {
     public static void main(String args[]){
             Scanner in = new Scanner(System.in);
         System.out.println("Enter number");
-       int arr[]=new int[5];
+       int arr[]=new int[6];
        for (int i=0;i<arr.length;i++){
             arr[i]=in.nextInt();
        }
